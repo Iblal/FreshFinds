656f772d0b07549721212f3b966bc47eb350df6b
iblalahmed12 <iblalahmed12@gmail.com>
2023-10-28 18:35:45 +0100

    feat(products-api) added create product endpoint

---
+++ FreshFinds.Product.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using FreshFinds.Product.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreshFinds.Product.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public static Task<ActionResult<ProductDto>> CreateProduct([FromBody] CreateProductDto productDto)
+        { 
+            return null;
+        }
+    }
+}
